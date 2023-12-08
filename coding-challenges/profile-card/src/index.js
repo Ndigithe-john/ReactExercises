3cbd63c356a28e4f679c935fa0b521457a8bf6d6
Ndigithe-john <ndigithejohn@gmail.com>
2023-12-08 15:28:03 -0800

    completed the first coding challenge

--- coding-challenges/profile-card/src/index.js
+++ coding-challenges/profile-card/src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -11,7 +9,89 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+  {
+    skill: "Docker",
+    level: "intermediate",
+    color: "#1D63ED",
+  },
+];
+
+
+function App(){
+  return (<div className='card'>
+  <Avatar/>
+  <div className='data'>
+<Intro/>
+<SkillsList/>
+  </div>
+
+  </div>
+  )
+}
+
+function Avatar(){
+  return (
+    <img className="avatar"src="assets/profileImg.jpg" alt='devProfile'/>
+  )
+}
+function Intro(){
+  return <main>
+    <h1>My Miss</h1>
+    <p>
+      Full-Stack Web developer and a Designer. when not coding i watch a movie, cook or take a walk. My best movie is Lord of the Rings and best series is Game of Thrones. The best lesson i have learnt in life is we all need Mentors. They are useful for when things get heavy/challenging. Not to carry them for us but to carry us in the process.
+    </p>
+  </main>
+}
+function SkillsList(){
+  
+  return <ul className='skill-list'>
+{skills.map(skill=> <Skill skillObject={skill} key={skill.skill}/>)}
+  </ul>
+}
+
+function Skill({skillObject}){
+  return (
+    <li className="skill" style={{ backgroundColor: skillObject.color }}>
+      <span>{skillObject.skill}</span>
+      <span>
+        {skillObject.level === "beginner" && "😩"}
+        {skillObject.level === "intermediate" && "🥳"}
+        {skillObject.level === "advanced" && "💪"}
+      </span>
+    </li>
+  );
+    
+  
+}
+
