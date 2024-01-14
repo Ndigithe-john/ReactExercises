d901d85dc3b2bc4418dbcb68449d4cdbdaa3e5fd
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-14 02:39:27 -0800

    handling state

--- state-prac/src/App.js
+++ state-prac/src/App.js
@@ -1,25 +1,39 @@
+import { useState } from "react";
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
   "Invest your new income 🤑",
 ];
 function App() {
-  const step = 2;
+  const [step, setStep] = useState(1);
+  function handlePreviousPress() {
+    if (step > 1) setStep(step - 1);
+  }
+  function nextPressHandler() {
+    if (step < 3) setStep(step + 1);
+  }
+
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step === 1 ? "active" : ""}`}> 1 </div>
-        <div className={`${step === 2 ? "active" : ""}`}> 2 </div>
-        <div className={`${step === 3 ? "active" : ""}`}> 3 </div>
+        <div className={`${step >= 1 ? "active" : ""}`}> 1 </div>
+        <div className={`${step >= 2 ? "active" : ""}`}> 2 </div>
+        <div className={`${step >= 3 ? "active" : ""}`}> 3 </div>
       </div>
       <p className="message">
         step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePreviousPress}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={nextPressHandler}
+        >
           Next
         </button>
       </div>
