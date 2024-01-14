f0209ca338c5a38f75d5ae6b76593f2aada17392
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-14 06:16:44 -0800

    ui of the challenge

--- coding-challenges/date-counter/src/App.js
+++ coding-challenges/date-counter/src/App.js
@@ -2,8 +2,25 @@ import "./App.css";
 
 function App() {
   return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+}
+function Counter() {
+  return (
     <div>
-      <h1>This is it</h1>
+      <div className="buttons">
+        <button>+</button>
+        <h6>Step:0</h6>
+        <button>-</button>
+      </div>
+      <div className="buttons">
+        <button>+</button>
+        <h6>Count:0</h6>
+        <button>-</button>
+      </div>
+      <p>try the app</p>
     </div>
   );
 }
