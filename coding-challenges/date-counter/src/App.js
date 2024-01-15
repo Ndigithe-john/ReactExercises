7271c006e3e4774924fec254c6375b00de0b36ce
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-15 05:32:49 -0800

    added count logic

--- coding-challenges/date-counter/src/App.js
+++ coding-challenges/date-counter/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
   return (
@@ -8,19 +9,33 @@ function App() {
   );
 }
 function Counter() {
+  const [count, setCount] = useState(0);
+  const [step, setStep] = useState(1);
+  function addCountHandler() {
+    setCount((prevCount) => prevCount + step);
+  }
+  function subtractCountHandler() {
+    setCount((prevCount) => prevCount - step);
+  }
+  function addStepCountHandler() {
+    setStep((prevstep) => prevstep + 1);
+  }
+  function subStepCountHandler() {
+    setStep((prestep) => prestep - 1);
+  }
   return (
     <div>
       <div className="buttons">
-        <button>+</button>
-        <h6>Step:0</h6>
-        <button>-</button>
+        <button onClick={subStepCountHandler}>-</button>
+        <h6>Step:{step}</h6>
+        <button onClick={addStepCountHandler}>+</button>
       </div>
       <div className="buttons">
-        <button>+</button>
-        <h6>Count:0</h6>
-        <button>-</button>
+        <button onClick={subtractCountHandler}>-</button>
+        <h6>Count:{count}</h6>
+        <button onClick={addCountHandler}>+</button>
       </div>
-      <p>try the app</p>
+      <p>Today is {Date().toLocaleLowerCase()}</p>
     </div>
   );
 }
