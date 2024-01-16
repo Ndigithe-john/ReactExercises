c26c75ac36f24469445bf7c98f7200c104c94da2
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-16 04:36:11 -0800

    building form and handling submission

--- travel-list/src/components/Form.js
+++ travel-list/src/components/Form.js
@@ -1,14 +1,17 @@
 import "../index.css";
 function Form() {
+  function handleFormSubmission(e) {
+    e.preventDefault();
+  }
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={handleFormSubmission}>
       <h3>What do you need for your trip😎</h3>
       <select>
-        <option value={1}>1</option>
-        <option value={2}>2</option>
-        <option value={3}>3</option>
-        <option value={4}>4</option>
-        <option value={5}>5</option>
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
       </select>
       <input type="text" placeholder="Item...." />
       <button>Add</button>
