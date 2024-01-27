d059da88be27cebb2e8c213bd62676bc81cb8493
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-27 04:25:30 -0800

    sorted my inputs

--- travel-list/src/components/PackingList.js
+++ travel-list/src/components/PackingList.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import "../index.css";
 // import initialItems from "../data/data";
 import Item from "./Item";
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  const [sortBy, setSortBy] = useState("input");
+  let sortedItem;
+  if (sortBy === "input") sortedItem = items;
+  if (sortBy === "description")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+  if (sortBy === "packed")
+    sortedItem = items
+      .slice()
+      .sort((a, b) => Number(a.packed) - Number(b.packed));
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {sortedItem.map((item) => (
           <Item
             item={item}
             key={item.id}
@@ -14,6 +26,13 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
