f9984dc7f311e6a518328656c0ea59432dfa779a
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-27 07:54:28 -0800

    final commit of the day

--- coding-challenges/accordion-challenge/src/components/Accordion.js
+++ coding-challenges/accordion-challenge/src/components/Accordion.js
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import "../index.css";
 import AccordionItem from "./AccordionItem";
 export default function Accordion({ data }) {
+  const [currentOpen, setCurrentlyOpen] = useState(null);
   return (
     <div className="accordion">
       {data.map((faq, index) => (
         <AccordionItem
+          currOpen={currentOpen}
+          onOpen={setCurrentlyOpen}
           key={index}
           title={faq.title}
-          text={faq.text}
           number={index}
-        />
+        >
+          {faq.text}
+        </AccordionItem>
       ))}
     </div>
   );
--- coding-challenges/accordion-challenge/src/components/AccordionItem.js
+++ coding-challenges/accordion-challenge/src/components/AccordionItem.js
@@ -1,16 +1,21 @@
-import { useState } from "react";
-export default function AccordionItem({ number, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
-
+export default function AccordionItem({
+  number,
+  title,
+  currOpen,
+  onOpen,
+  children,
+}) {
+  const isOpen = number === currOpen;
   function handleToggle() {
-    setIsOpen((current) => !current);
+    // setIsOpen((current) => !current);
+    onOpen(number);
   }
   return (
     <div className={`item ${isOpen ? "open" : ""}`} onClick={handleToggle}>
       <p className="number">{number < 9 ? `0${number + 1}` : number + 1}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen && <div className="content-box">{text}</div>}
+      {isOpen && <div className="content-box">{children}</div>}
     </div>
   );
 }
