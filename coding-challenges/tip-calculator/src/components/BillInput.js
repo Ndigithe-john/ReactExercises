80c7e519b3fc8ac21f96a2871272f44cedb631e0
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-28 04:48:03 -0800

    added state to control the input bill amount

--- coding-challenges/tip-calculator/src/components/BillInput.js
+++ coding-challenges/tip-calculator/src/components/BillInput.js
@@ -1,9 +1,15 @@
+import { useState } from "react";
 import "../index.css";
 function BillInput() {
+  const [billAmount, setBillAmount] = useState(0);
   return (
     <div>
       <label>How much was the bill? </label>
-      <input type="text" />
+      <input
+        type="number"
+        value={billAmount}
+        onChange={(e) => setBillAmount(e.target.value)}
+      />
     </div>
   );
 }
