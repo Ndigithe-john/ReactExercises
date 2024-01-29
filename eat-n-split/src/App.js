99229324a404d8381f79270245237bdee0905187
Ndigithe-john <ndigithejohn@gmail.com>
2024-01-29 07:14:23 -0800

    completed the expense split bill app

--- eat-n-split/src/App.js
+++ eat-n-split/src/App.js
@@ -23,6 +23,16 @@ function App() {
     setShowAddFriend(false);
     // setSelectedFriend(friend);
   }
+  function handleSplitBill(value) {
+    setFriends((friends) =>
+      friends.map((friend) =>
+        friend.id === selectedFriend.id
+          ? { ...friend, balance: friend.balance + value }
+          : friend
+      )
+    );
+    setSelectedFriend(null);
+  }
   return (
     <div className="app">
       <div className="sidebar">
@@ -37,7 +47,12 @@ function App() {
           {showAddFriend ? "Close" : "Add friend"}
         </Button>
       </div>
-      {selectedFriend && <FormSplitBill selectedFriend={selectedFriend} />}
+      {selectedFriend && (
+        <FormSplitBill
+          selectedFriend={selectedFriend}
+          onSplitBill={handleSplitBill}
+        />
+      )}
     </div>
   );
 }
