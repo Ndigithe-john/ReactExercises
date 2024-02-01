e37e60ba96cb12f196fe828d360d0420a283e21c
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-01 11:59:24 -0800

    Creating rating stars

--- usepopcorn/src/components/StarRating.js
+++ usepopcorn/src/components/StarRating.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import Star from "./Star";
 const containerStyle = {
   display: "flex",
   alignItems: "center",
@@ -5,21 +7,21 @@ const containerStyle = {
 };
 const starContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 const textStyle = {
   lineHeight: "1",
   margin: "0",
 };
 export default function StarRating({ maxRating = 5 }) {
+  const { rating, setRating } = useState(0);
   return (
     <div style={containerStyle}>
       <div style={starContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span>S{i + 1}</span>
+          <Star key={i} onClick={() => setRating(i + 1)} />
         ))}
       </div>
-      <p style={textStyle}>10</p>
+      <p style={textStyle}>{rating || ""}</p>
     </div>
   );
 }
