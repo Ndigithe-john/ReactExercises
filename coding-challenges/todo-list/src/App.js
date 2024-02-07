c5a70fe766cae8a0a5eeebae8f3d790fe102fc6c
Ndigithe-john <ndigithejohn@gmail.com>
2024-02-07 10:39:17 -0800

    changes to the data storage

--- coding-challenges/todo-list/src/App.js
+++ coding-challenges/todo-list/src/App.js
@@ -4,16 +4,18 @@ import Tasks from "./components/Tasks";
 import "./index.css";
 function App() {
   const [task, getTask] = useState("");
+  const [tasks, setTasks] = useState([]);
   function submitHandler(e) {
     e.preventDefault();
     if (!task) return;
     const newTask = { task, completed: true, id: Date() };
+    setTasks((prev) => [...prev, newTask]);
     getTask("");
   }
   return (
     <div className="App">
       <Form task={task} onGetTask={getTask} submitHandler={submitHandler} />
-      <Tasks task={task} />
+      <Tasks task={task} tasks={tasks} />
     </div>
   );
 }
