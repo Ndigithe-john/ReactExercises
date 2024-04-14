2070aa7019e9ced3f0c68fb42555ea3aad9cc9ab
Ndigithe-john <ndigithejohn@gmail.com>
2024-04-14 11:36:00 +0300

    changes on the app

--- usepopcorn/src/components/SelectedMovie.js
+++ usepopcorn/src/components/SelectedMovie.js
@@ -1,9 +1,38 @@
+import { useEffect, useState } from "react";
+const KEY = "308c28b2";
 const SelectedMovie = ({ selectedId, onCloseMovie }) => {
+  const [movie, setMovie] = useState({});
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actor: actor,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=${KEY}&i=${selectedId}`
+      );
+      const data = await response.json();
+      setMovie(data);
+    }
+    getMovieDetails();
+  }, []);
   return (
     <div className="details">
-      <button className="btn-back" onClick={onCloseMovie}>
-        &larr;
-      </button>
+      <headr>
+        <button className="btn-back" onClick={onCloseMovie}>
+          &larr;
+        </button>
+        <img src={poster} alt={title} />
+      </headr>
       {selectedId}
     </div>
   );
