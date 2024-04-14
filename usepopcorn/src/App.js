69917fd4fa6ede90d65ea4ee8aaaed994b611344
Ndigithe-john <ndigithejohn@gmail.com>
2024-04-14 15:48:22 +0300

    added the clean up function to http fetch function

--- usepopcorn/src/App.js
+++ usepopcorn/src/App.js
@@ -45,12 +45,14 @@ export default function App() {
     );
   }
   useEffect(() => {
+    const controller = new AbortController();
     async function fetchMovies() {
       try {
         setIsLoading(true);
         setError("");
         const res = await fetch(
-          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
+          `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`,
+          { signal: controller.signal }
         );
 
         if (!res.ok) {
@@ -61,9 +63,12 @@ export default function App() {
           throw new Error("Movie not found");
         }
         setMovies(data.Search);
+        setError("");
       } catch (err) {
         console.error(err.message);
-        setError(err.message);
+        if (err.name !== "AbortError") {
+          setError(err.message);
+        }
       } finally {
         setIsLoading(false);
       }
@@ -74,6 +79,9 @@ export default function App() {
       return;
     }
     fetchMovies();
+    return function () {
+      controller.abort();
+    };
   }, [query]);
 
   return (
