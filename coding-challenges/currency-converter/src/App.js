fe6e3b82ae134cd6f895fa10c55b2703f3f58425
Ndigithe-john <ndigithejohn@gmail.com>
2024-04-16 00:13:14 +0300

    adding the state to manage change

--- coding-challenges/currency-converter/src/App.js
+++ coding-challenges/currency-converter/src/App.js
@@ -1,16 +1,24 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
+import { useState } from "react";
 
 export default function App() {
+  const [inputAmount, setInputAmount] = useState("");
+  const [fromAmount, setFromAmount] = useState("USD");
+  const [convertTo, setConvertTo] = useState("USD");
   return (
     <div>
-      <input type="text" />
-      <select>
+      <input
+        type="text"
+        value={inputAmount}
+        onChange={(e) => setInputAmount(Number(e.target.value))}
+      />
+      <select onChange={(e) => setFromAmount(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
         <option value="INR">INR</option>
       </select>
-      <select>
+      <select onChange={(e) => setConvertTo(e.target.value)}>
         <option value="USD">USD</option>
         <option value="EUR">EUR</option>
         <option value="CAD">CAD</option>
