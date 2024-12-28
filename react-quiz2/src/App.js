ff431e6110a14b1dba8611796a14a04cb65133ed
Ndigithe-john <ndigithejohn@gmail.com>
2024-12-28 23:00:03 +0300

    added a fake api for my questions

--- react-quiz2/src/App.js
+++ react-quiz2/src/App.js
@@ -1,6 +1,42 @@
-import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+
+const initialState = {
+  questions: {},
+  //loading, error,ready,active,finished
+  status: "loading",
+};
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    default:
+      throw new Error("Unknown action");
+  }
+}
 function App() {
-  return <DateCounter />;
+  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => {
+        dispatch({ type: "dataReceived", payload: data });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      <Main className="main">
+        <p>1/15</p>
+        <p>Question</p>
+      </Main>
+    </div>
+  );
 }
 
 export default App;
