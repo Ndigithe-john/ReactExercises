1ac07003506966d6baa4073ddc71441d77846c4d
Ndigithe-john <ndigithejohn@gmail.com>
2024-12-28 23:29:37 +0300

    rendering the main components as the status changes

--- react-quiz2/src/App.js
+++ react-quiz2/src/App.js
@@ -1,6 +1,10 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: {},
@@ -11,13 +15,17 @@ function reducer(state, action) {
   switch (action.type) {
     case "dataReceived":
       return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
       throw new Error("Unknown action");
   }
 }
 function App() {
   const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
-
+  const numQuestions = questions.length;
   useEffect(() => {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
@@ -26,14 +34,20 @@ function App() {
       })
       .catch((err) => {
         console.log(err);
+        dispatch({ type: "dataFailed" });
       });
   }, []);
+
   return (
     <div className="app">
       <Header />
       <Main className="main">
-        <p>1/15</p>
-        <p>Question</p>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question />}
       </Main>
     </div>
   );
