860eecb439a6ca81058d08744d1c2f5816efd1b4
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-03 12:36:39 +0300

    adding action creator functions

--- redux-intro/src/store.js
+++ redux-intro/src/store.js
@@ -1,3 +1,5 @@
+import { createStore } from "redux";
+
 const initialState = {
   balance: 0,
   loan: 0,
@@ -8,12 +10,17 @@ function reducer(state = initialState, action) {
   switch (action.type) {
     case "account/deposit":
       return { ...state, balance: state.balance + action.payload };
-    case "account/withdrawal":
+    case "account/withdraw":
       return { ...state, balance: state.balance - action.payload };
     case "account/requestLoan":
       if (state.loan > 0) return state;
-      return { ...state, loan: action.payload };
-    case "accout/payload":
+      return {
+        ...state,
+        loan: action.payload.amount,
+        loanPurpose: action.payload.purpose,
+        balance: state.balance + action.payload.amount,
+      };
+    case "account/payLoan":
       return {
         ...state,
         loan: 0,
@@ -24,3 +31,39 @@ function reducer(state = initialState, action) {
       return state;
   }
 }
+
+const store = createStore(reducer);
+store.subscribe(() => {
+  console.log("Store updated", store.getState());
+});
+// store.dispatch({ type: "account/deposit", payload: 500 });
+
+// store.dispatch({ type: "account/withdraw", payload: 200 });
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "buy a car" },
+// });
+
+// store.dispatch({ type: "account/payLoan" });
+
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return { type: "account/requestLoan", payload: { amount, purpose } };
+}
+
+function payloan() {
+  return { type: "account/payLoan" };
+}
+
+store.dispatch(deposit(500));
+store.dispatch(withdraw(200));
+store.dispatch(requestLoan(2000, "Buy more crypto"));
+store.dispatch(payloan());
