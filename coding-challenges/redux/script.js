a060a0cc1728848daf51c2980a278134b960f4c6
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-04 16:46:43 +0300

    redux setup

--- coding-challenges/redux/script.js
+++ coding-challenges/redux/script.js
@@ -1,1 +1,44 @@
-console.log("Welcome to redux");
+const redux = require("redux");
+const createStore = redux.createStore;
+
+const ORDER_PIZZA = "ORDER_PIZZA";
+
+//action
+const action = {
+  type: ORDER_PIZZA,
+  shop_name: "Pizza shop",
+};
+
+//Action creator
+function orderPizza() {
+  return {
+    type: ORDER_PIZZA,
+    shop_name: "Pizza shop",
+  };
+}
+
+//reducer
+
+const initialState = {
+  pizzaBase: 100,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ORDER_PIZZA:
+      return {
+        ...state,
+        pizzaBase: state.pizzaBase - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+//STORE
+// 1. Store needs to hold application state
+
+const store = createStore(reducer);
+// 2. It exposes a method called getState which gives your application access to the current state in the store
+
+console.log(store.getState());
