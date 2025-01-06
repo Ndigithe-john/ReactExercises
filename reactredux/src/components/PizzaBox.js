7d60977d5d737a4ab6e035169296b34c89964a32
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-06 13:28:36 +0300

    created the ui

---
+++ reactredux/src/components/PizzaBox.js
@@ -0,0 +1,10 @@
+function PizzaBox() {
+  return (
+    <div className="container">
+      <h2 className="text">Number of pizza Base available - 100</h2>
+      <button className="btn">Order Pizza</button>
+    </div>
+  );
+}
+
+export default PizzaBox;
