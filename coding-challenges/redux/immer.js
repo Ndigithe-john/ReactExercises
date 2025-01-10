836493ce02ae65d65d36aa910356c4ee77ad7d23
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-10 22:57:40 +0300

    added the loger function

--- coding-challenges/redux/immer.js
+++ coding-challenges/redux/immer.js
@@ -1,3 +1,11 @@
+const redux = require("redux");
+const reduxLogger = require("redux-logger");
+const produce = require("immer").produce;
+const createStore = redux.createStore;
+
+const applyMiddleware = redux.applyMiddleware;
+const logger = reduxLogger.createLogger();
+
 const FILL_UPDATE = "FILL_UPDATE";
 
 const initialState = {
@@ -26,5 +34,18 @@ const reducer = (state = initialState, action) => {
           filling: action.payload,
         },
       };
+    default:
+      return state;
   }
 };
+
+const store = createStore(reducer, applyMiddleware(logger));
+console.log("initial state", store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("updated state", store.getState());
+});
+
+store.dispatch(updateFill("Grilled Vegies and cheese"));
+
+unsubscribe();
