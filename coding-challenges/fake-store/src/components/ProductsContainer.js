00aa4e792e9147b0a180c3f09a0eaeddb08c2bcc
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-12 01:06:36 +0300

    added the grid to render the products

--- coding-challenges/fake-store/src/components/ProductsContainer.js
+++ coding-challenges/fake-store/src/components/ProductsContainer.js
@@ -4,6 +4,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { fetchProducts } from "../actions/productActions";
 import ProductCard from "./ProductCard";
 import Loader from "./Loader";
+import { Grid, GridItem } from "@chakra-ui/react";
 
 function ProductsContainer() {
   const productsData = useSelector((products) => products.products);
@@ -20,7 +21,13 @@ function ProductsContainer() {
       {isLoading ? (
         <Loader />
       ) : (
-        productsData.map((product) => <ProductCard product={product} />)
+        <Grid templateColumns="repeat(4, 1fr)" gap={4}>
+          {productsData.map((product) => (
+            <GridItem key={product.id}>
+              <ProductCard product={product} />
+            </GridItem>
+          ))}
+        </Grid>
       )}
     </>
   );
