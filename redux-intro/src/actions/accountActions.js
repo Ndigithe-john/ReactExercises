77ca1b53c09f684d652dce3a188e9fdeb5071b87
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-13 00:00:44 +0300

    added the logic for converting currency

--- redux-intro/src/actions/accountActions.js
+++ redux-intro/src/actions/accountActions.js
@@ -3,10 +3,22 @@ import {
   ACCOUNT_PAYLOAN,
   ACCOUNT_WITHDRAW,
   ACCOUNT_REQUEST_LOAN,
+  ACCOUNT_CURRENCY_CONVERT,
 } from "../constants/accountsConstants";
 
-export const accountDeposit = (amount) => {
-  return { type: ACCOUNT_DEPOSIT, payload: amount };
+export const accountDeposit = (amount, currency) => {
+  if (currency === "USD") return { type: ACCOUNT_DEPOSIT, payload: amount };
+
+  return async function (dispatch, getState) {
+    dispatch({ type: ACCOUNT_CURRENCY_CONVERT });
+    const res = await fetch(
+      `https://api.frankfurter.dev/v1/latest?base=${currency}&symbols=USD`
+    );
+    const data = await res.json();
+    const convertedAmount = +(data.rates.USD * amount).toFixed(2);
+
+    dispatch({ type: ACCOUNT_DEPOSIT, payload: convertedAmount });
+  };
 };
 
 export const accountWithdraw = (amount) => {
--- redux-intro/src/constants/accountsConstants.js
+++ redux-intro/src/constants/accountsConstants.js
@@ -2,3 +2,4 @@ export const ACCOUNT_DEPOSIT = "account/deposit";
 export const ACCOUNT_WITHDRAW = "account/withdraw";
 export const ACCOUNT_REQUEST_LOAN = "account/requestLoan";
 export const ACCOUNT_PAYLOAN = "account/paylaon";
+export const ACCOUNT_CURRENCY_CONVERT = "account/convertingCurrency";
