0c568c884960a28f09e209ccba65fa34193e4628
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-12 23:03:03 +0300

    added the changes in my redux to accomodate thunk middleware

--- redux-intro/src/store-v1.js
+++ redux-intro/src/store-v1.js
@@ -1,4 +1,8 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import { thunk } from "redux-thunk";
+import { composeWithDevTools } from "@redux-devtools/extension";
+import logger from "redux-logger";
+
 import { customerAccountReducer } from "./reducers/accountsReducers";
 import { customerReducer } from "./reducers/customerReducer";
 
@@ -7,6 +11,9 @@ const rootReducer = combineReducers({
   customer: customerReducer,
 });
 
-const storeV1 = createStore(rootReducer);
+const storeV1 = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
 
 export default storeV1;
