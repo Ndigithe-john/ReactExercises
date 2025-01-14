81f64a276b4ce60532a830aa0aadfa1f7dda96f2
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-14 22:12:59 +0300

    added the paths to my routes

--- fast-pizza/src/App.jsx
+++ fast-pizza/src/App.jsx
@@ -1,5 +1,38 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import Home from "@components/Home";
+import Menu from "@features/menu/Menu";
+import Cart from "@features/cart/Cart";
+import CreateOrder from "@features/order/CreateOrder";
+import Order from "@features/order/Order";
+import NotFound from "@components/Error";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <NotFound />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+  {
+    path: "/order/new",
+    element: <CreateOrder />,
+  },
+  {
+    path: "order/:orderId",
+    element: <Order />,
+  },
+]);
+
 function App() {
-  return <div>Hello vite</div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
