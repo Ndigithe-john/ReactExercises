191ed6150173eeb2fbb693923fab798b5aae9edf
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-15 13:06:52 +0300

    added the loader for getting order

--- fast-pizza/src/App.jsx
+++ fast-pizza/src/App.jsx
@@ -4,14 +4,13 @@ import Home from "@components/Home";
 import Menu, { loader as menuLoader } from "@features/menu/Menu";
 import Cart from "@features/cart/Cart";
 import CreateOrder from "@features/order/CreateOrder";
-import Order from "@features/order/Order";
+import Order, { loader as orderLoader } from "@features/order/Order";
 import NotFound from "@components/Error";
 import AppLayout from "@components/AppLayout";
 
 const router = createBrowserRouter([
   {
     element: <AppLayout />,
-    errorElement: <NotFound />,
     children: [
       {
         path: "/",
@@ -21,6 +20,7 @@ const router = createBrowserRouter([
         path: "/menu",
         element: <Menu />,
         loader: menuLoader,
+        errorElement: <NotFound />,
       },
       {
         path: "/cart",
@@ -33,6 +33,8 @@ const router = createBrowserRouter([
       {
         path: "order/:orderId",
         element: <Order />,
+        loader: orderLoader,
+        errorElement: <NotFound />,
       },
     ],
   },
--- fast-pizza/src/components/Header.jsx
+++ fast-pizza/src/components/Header.jsx
@@ -1,9 +1,11 @@
+import SearchOrder from "@features/order/SearchOrder";
 import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
 
       <p>John</p>
     </header>
