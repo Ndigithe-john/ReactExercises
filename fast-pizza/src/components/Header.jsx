748aaceaac9c23bc97a2e1be0897f7a83ad3265e
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-18 14:58:44 +0300

    added more styling to my menu

--- fast-pizza/src/components/Header.jsx
+++ fast-pizza/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b-2 border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b-2 border-stone-300 bg-yellow-500 px-4 py-3 font-serif uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast React Pizza Co.
       </Link>
--- fast-pizza/src/features/cart/Cart.jsx
+++ fast-pizza/src/features/cart/Cart.jsx
@@ -36,8 +36,10 @@ function Cart() {
       <h2>Your cart, %NAME%</h2>
 
       <div>
-        <Button to="/order/new">Order pizzas</Button>
-        <button>Clear cart</button>
+        <Button type="primary" to="/order/new">
+          Order pizzas
+        </Button>
+        <buttonn type="primary">Clear cart</buttonn>
       </div>
     </div>
   );
