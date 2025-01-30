4ae627f6d0bedb43b8bac6a38322fdc151070175
Ndigithe-john <ndigithejohn@gmail.com>
2025-01-30 16:00:20 +0300

    added the react query to my application

--- the-wild-oasis/src/App.jsx
+++ the-wild-oasis/src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import GlobalStyles from "@styles/GlobalStyles";
 
@@ -12,9 +14,18 @@ import NewUsers from "@pages/Users";
 import AppLayout from "@ui/AppLayout";
 import Cabins from "@pages/Cabins";
 
+const queryclient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 60 * 1000,
+    },
+  },
+});
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryclient}>
+      <ReactQueryDevtools initialIsOpen={false} />
       <GlobalStyles />
       <BrowserRouter>
         <Routes>
@@ -32,7 +43,7 @@ function App() {
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </>
+    </QueryClientProvider>
   );
 }
 
