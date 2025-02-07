bdb39ae3b37bc4131495a43734fbdf3c9e986126
Ndigithe-john <ndigithejohn@gmail.com>
2025-02-07 12:32:10 +0300

    created a counter compund component

--- coding-challenges/compound-pattern/src/App.js
+++ coding-challenges/compound-pattern/src/App.js
@@ -1,11 +1,23 @@
 import Counter from "./Counter";
 
-function App() {
+export default function App() {
   return (
-    <>
-      <Counter />
-    </>
+    <div>
+      <h1>Compound Component Pattern</h1>
+      {/* <Counter
+        iconIncrease="+"
+        iconDecrease="-"
+        label="My NOT so flexible counter"
+        hideLabel={false}
+        hideIncrease={false}
+        hideDecrease={false}
+      /> */}
+      <Counter>
+        <Counter.Label>My super flexible counter</Counter.Label>
+        <Counter.Decrease icon="➖" />
+        <Counter.Count />
+        <Counter.Increase icon="➕" />
+      </Counter>
+    </div>
   );
 }
-
-export default App;
--- coding-challenges/compound-pattern/src/Counter.js
+++ coding-challenges/compound-pattern/src/Counter.js
@@ -1,5 +1,46 @@
-function Counter() {
-  return <div></div>;
+import { createContext, useContext, useState } from "react";
+
+// Step 1: Create a context
+const CounterContext = createContext();
+
+// Step 2: Create a parent compoent
+function Counter({ children }) {
+  const [count, setCount] = useState(0);
+
+  const increase = () => setCount((c) => c + 1);
+  const decrease = () => setCount((c) => c - 1);
+
+  return (
+    <CounterContext.Provider value={{ count, increase, decrease }}>
+      <span>{children}</span>
+    </CounterContext.Provider>
+  );
 }
+
+// Step 3: Create child compoents to help implementing the common task
+
+function Count() {
+  const { count } = useContext(CounterContext);
+  return <span>{count}</span>;
+}
+function Label({ children }) {
+  return <span>{children}</span>;
+}
+function Increase({ icon }) {
+  const { increase } = useContext(CounterContext);
+  return <button onClick={increase}>{icon}</button>;
+}
+function Decrease({ icon }) {
+  const { decrease } = useContext(CounterContext);
+
+  return <button onClick={decrease}>{icon}</button>;
+}
+
+//  Step 4: Add the child compoennts as properties to the parent component
+
+Counter.Count = Count;
+Counter.Label = Label;
+Counter.Increase = Increase;
+Counter.Decrease = Decrease;
 
 export default Counter;
