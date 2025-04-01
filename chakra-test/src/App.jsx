165fafc2ed063735d1ca06e7ddb7bf1e799482c0
Ndigithe-john <ndigithejohn@gmail.com>
2025-04-01 22:45:32 +0300

    new app

---
+++ chakra-test/src/App.jsx
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Flex as="h1" bg="green" justifyContent="center" alignItems="center">
+      This is it
+    </Flex>
+  );
+}
+
+export default App;
