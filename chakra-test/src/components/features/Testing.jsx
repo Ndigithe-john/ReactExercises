6de8d4e8d12fbe5b804207f1335b8d4fed441c87
Ndigithe-john <ndigithejohn@gmail.com>
2025-04-04 23:48:01 +0300

    t

--- chakra-test/src/components/features/Testing.jsx
+++ chakra-test/src/components/features/Testing.jsx
@@ -1,14 +1,23 @@
-import { AspectRatio } from "@chakra-ui/react";
+import { AspectRatio, Box } from "@chakra-ui/react";
 
 function Testing() {
   return (
-    <AspectRatio maxW="400px" ratio={1}>
-      <iframe
-        title="naruto"
-        src="https://www.youtube.com/embed/QhBnZ6NPOY0"
-        allowFullScreen
-      />
-    </AspectRatio>
+    // <AspectRatio maxW="400px" ratio={1}>
+    //   <iframe
+    //     title="naruto"
+    //     src="https://www.youtube.com/embed/QhBnZ6NPOY0"
+    //     allowFullScreen
+    //   />
+    // </AspectRatio>
+
+    <Box
+      p="4"
+      borderWidth="1px"
+      borderColor="border.disabled"
+      color="fg.disabled"
+    >
+      Somewhat disabled box
+    </Box>
   );
 }
 
