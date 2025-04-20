86d21f9589505243da54cc7e2c5d3ba4bd781bf1
Ndigithe-john <ndigithejohn@gmail.com>
2025-04-20 21:57:42 +0300

    added data to by server

---
+++ store/backend/seeder.js
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+import colors from "colors";
+
+import connectDB from "./config/db.config.js";
+
+dotenv.config();
+
+async function importData() {
+  try {
+  } catch (error) {}
+}
